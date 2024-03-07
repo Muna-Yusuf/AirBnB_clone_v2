598f1d34e4a2bd47357b942e714fbc1da8bd72fb
Muna-Yusuf <muna.y.ajim@gmail.com>
2024-03-07 11:52:47 -0800

    ...

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,44 +2,39 @@
 #Sets up your web servers for the deployment of web_static.
 
 #Update and install nginx.
-apt-get -y update && sudo apt-get -y upgrade
-apt-get -y install nginx
+sudo apt-get -y update && sudo apt-get -y upgrade
+sudo apt-get -y install nginx
 
 #Creat folders "data, web_static, releases, shared, test"
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p "/data/web_static/releases/test/"
+sudo mkdir -p "/data/web_static/shared/"
 
 #Creat html file && symbolic link(test, current)
-echo "testing Nginx configuration" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+contant="Holberton School"
+html_content="<html>
+  <head>
+  </head>
+  <body>
+    $contant
+  </body>
+</html>"
+
+echo "$html_content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+rm -rf data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 #Give ownership of the /data/ folder to the ubuntu user AND group.
-chown -R ubuntu:ubuntu /data/
-chgrp -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 #Update the Nginx configuration.
-printf %s "server{
-	listen 80 defualut_server;
-	add_header X-served-By $hostname;
-	root  /var/www/html;
-	index index.html index.html;
-	
-	location /hbnb_static {
-	alias /data/web_static/current/;
-	index index.html index.html;
-	}
-
-	location /redirect_me {
-	return 301 https://github.com/Muna-Yusuf; 
-	}
-
-	error_page 404 /404.html;
-
-	location /404 {
-	root /var/www/html;
-	internal;
-	}
-}" > /etc/nginx/sites-available/default
+sudo wget -q -o etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
+config="/etc/nginx/sites-available/default"
+echo "Holberton School Hello Worl" | sudo tee /var/www/html/index.html > /dev/null
+sudo sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' $config
+sudo sed -i '/^}$/i \ \n\tlocation @404 {return 404 "Ceci n'\''est pas une page\\n";}' $config
+sudo sed -i 's/=404/@404/g' $config
+sudo sed -i "/^server {/a \ \tadd_header X-Served-By $HOSTNAME;" $config
+sudo sed -i '/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}' $config
 
 #Restart Nginx
-service nginx restart
--- data/p.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo chown -R ubuntu /data/
