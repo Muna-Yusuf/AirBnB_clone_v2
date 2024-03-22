137f4a37d2240153cdaeb21fd03243eb051242a7
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-03-21 17:02:07 -0700

    ...

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,33 +1,88 @@
 # Redo task 0 using puppet
 
-exec { 'updata':
-  command => '/usr/bin/apt-get update',
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
--> package { 'nginx':
-  ensure => installed,
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
--> exec { 'mkdir':
-  command => "/usr/bin/mkdir -p '/data/web_static/releases/test/' '/data/web_static/shared/'"
-}
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
 
--> exec { 'simple content':
-  command => '/usr/bin/echo "simple content!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
-}
--> exec { 'rm':
-  command => '/usr/bin/rm -rf /data/web_static/current',
-}
--> exec { 'symbolic link':
-  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
-}
--> exec { 'chown':
-  command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
-}
--> exec { 'hbnb_static':
-  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/; index index.html;}" /etc/nginx/sites-enabled/default',
-  provider => shell,
-}
--> exec { 'restart nginx':
-  command => '/usr/sbin/service nginx restart',
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
