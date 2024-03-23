39332c95e79ed1365e7133e2b6f1381716cbf6ba
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-03-23 16:54:40 -0700

    ....

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,27 @@
 #!/usr/bin/env bash
-#Sets up your web servers for the deployment of web_static.
-
-#Update and install nginx.
-sudo apt-get -y update && sudo apt-get -y upgrade
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-#Creat folders "data, web_static, releases, shared, test"
-sudo mkdir -p "/data/web_static/releases/test/"
-sudo mkdir -p "/data/web_static/shared/"
-
-#Creat html file && symbolic link(test, current)
-
-contant="Holberton School"
-html_content="<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
-    $contant
+    Holberton School
   </body>
-</html>"
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-echo "$html_content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-#Give ownership of the /data/ folder to the ubuntu user AND group.
 sudo chown -R ubuntu:ubuntu /data/
 
-#Update the Nginx configuration.
-sudo wget -q -o /etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
-config="/etc/nginx/sites-available/default"
-echo "Holberton School Hello Worl" | sudo tee /var/www/html/index.html > /dev/null
-sudo sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' $config
-sudo sed -i '/^}$/i \ \n\tlocation @404 {return 404 "Ceci n'\''est pas une page\\n";}' $config
-sudo sed -i 's/=404/@404/g' $config
-sudo sed -i "/^server {/a \ \tadd_header X-Served-By $HOSTNAME;" $config
-sudo sed -i '/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}' $config
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-#Restart Nginx
+sudo service nginx restart
