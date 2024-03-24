a922918def12c9bab66e5e2099063029036964fb
Muna-Yusuf <muna.y.a.jim@gmail.com>
2024-03-24 22:03:13 +0000

    ...

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Stating flask app. """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Script that starts a Flask web application."""
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Script that displays HBNB."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_text(text):
+    """Script that displays "C" followed by the value of the text variable."""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
