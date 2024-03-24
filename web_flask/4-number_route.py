a72a6a952cbe4dd945034896dde2ff8937e4715c
Muna-Yusuf <muna.y.a.jim@gamil.com>
2024-03-24 23:46:25 +0000

    ...

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,36 +4,36 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """Script that starts a Flask web application."""
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Script that displays HBNB."""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def C_text(text):
     """Script that displays "C" followed by the value of the text variable."""
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """Script displays"Python"followed by the value of the text variable."""
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """Script displays n is a number only if n is an integer."""
-    return '{} is a number'.format(n)
+    return "{:d} is a number".format(n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
