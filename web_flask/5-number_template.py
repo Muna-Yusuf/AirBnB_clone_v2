ceb5195541c64d4e10644bd486ce090b65d13cd4
Muna-Yusuf <muna.y.a.jim@gamil.com>
2024-03-25 00:04:42 +0000

    ...

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,8 +38,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """Script displays display a HTML page only if n is an integer."""
-    path = "5-number.html"
-    return render_template(path, n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
